af2d4df30215e5c85240a3a49ee8ec8e57840692
Jeong Dong <eejxdong99@gmail.com>
2022-10-31 11:13:56 +0900

    ch08 product_list

---
+++ jspbook/src/main/java/jspbook/ch08/Product.java
@@ -0,0 +1,19 @@
+package jspbook.ch08;
+
+public class Product {
+	private String[] productList = {"item1(핸드폰)","item2(노트북)","item3(시계)","item4(오디오)","item5(컴퓨터)"};
+	private int num1 = 10;
+	private int num2 = 20;
+	
+	public String[] getProductList() {
+		return productList;
+	}
+	public int getNum1() {
+		return num1;
+	}
+	public int getNum2() {
+		return num2;
+	}
+	
+	
+}
