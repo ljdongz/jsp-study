ad7eddc08fb85c2419eefc970e297d3f2a6c71d0
Jeong Dong <eejxdong99@gmail.com>
2022-11-14 11:15:48 +0900

    ch10 listener

---
+++ jspbook/src/main/java/jspbook/ch10/TestContextListener.java
@@ -0,0 +1,63 @@
+package jspbook.ch10;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class TestContextListener
+ *
+ */
+@WebListener
+public class TestContextListener implements ServletContextAttributeListener, ServletContextListener {
+
+	/**
+	 * Default constructor.
+	 */
+	public TestContextListener() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
+	 */
+	public void attributeAdded(ServletContextAttributeEvent scae) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
+	 */
+	public void attributeRemoved(ServletContextAttributeEvent scae) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+	 */
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
+	 */
+	public void attributeReplaced(ServletContextAttributeEvent scae) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	ServletContext ctx = sce.getServletContext();
+    	Book mybook = new Book("컴퓨터이야기", "TUK", 40000, "한공대출판사"); 
+    	ctx.setAttribute("book", mybook); 
+    	System.out.println("TestContextListener 초기화되었습니다");
+    }
+
+}
