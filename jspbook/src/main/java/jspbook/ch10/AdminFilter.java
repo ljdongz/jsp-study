aaaa216c4aa88be299e153aff31f1c6acbcf92f6
Jeong Dong <eejxdong99@gmail.com>
2022-11-16 11:12:40 +0900

    ch10: filter

---
+++ jspbook/src/main/java/jspbook/ch10/AdminFilter.java
@@ -0,0 +1,58 @@
+package jspbook.ch10;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+/**
+ * Servlet Filter implementation class AdminFilter
+ */
+@WebFilter("/ch10/admin/*")
+public class AdminFilter extends HttpFilter implements Filter {
+
+	Properties p;
+
+	public AdminFilter() {
+
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		System.out.println("AdminFilter : Start ");
+		p = (Properties) request.getServletContext().getAttribute("prop");
+		p.put("adminId", "admin");
+		request.setAttribute("tel", "010-1234-1234");
+		// 필터처리 종료, 다음 필터 실행
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
